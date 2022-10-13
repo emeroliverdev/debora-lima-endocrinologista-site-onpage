73e33b43d098a16d9ae545ce8b54cb2cf805c73a
Emerson Oliveira <emersonoliveira.contato@gmail.com>
2022-10-13 09:40:25 -0300

    Alterações iniciais do projeto

---
+++ site-debora-lima/src/componentes/header.jsx
@@ -0,0 +1,19 @@
+function Header() {
+  return (
+    <header className="header">
+      <div className="flex flex-col pb-20">
+        <span className="span-header">
+          <strong>CUIDADO</strong> e <b>CARINHO</b> com as
+        </span>
+        <span className="span-header">crianças nos tratamentos de</span>
+        <h1 className="pl-2 pt-2">
+          <span className="text-[45px] font-poppins leading-[50px] text-[#E92267] pl-6 font-bold">
+            {" "}
+          </span>
+        </h1>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
