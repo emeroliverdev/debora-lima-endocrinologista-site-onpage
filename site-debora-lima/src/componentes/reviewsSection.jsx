550b06493b3f66d6d0166be2671921aa660e4391
Emerson Oliveira <emersonoliveira.contato@gmail.com>
2022-10-17 08:03:29 -0300

    Inserção das imagens dos depoimentos

--- site-debora-lima/src/componentes/reviewsSection.jsx
+++ site-debora-lima/src/componentes/reviewsSection.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import Fade from "react-reveal/Fade";
+import Avaliacao1 from "../imagens/avaliação_gmn_01.png";
+import Avaliacao2 from "../imagens/avaliação_gmn_02.png";
+import Avaliacao3 from "../imagens/avaliação_gmn_03.png";
 
 function reviewsSection() {
   return (
@@ -15,6 +18,31 @@ function reviewsSection() {
           {" "}
         </div>
       </Fade>
+      <div>
+        <div className="w-[767px] flex justify-center mt-10">
+          <a href="https://bit.ly/3CCmlfi">
+            <img
+              src={Avaliacao1}
+              alt="avaliação do perfil google meu negocio"
+              className="w-[650px]"
+            />
+          </a>
+          <a href="https://bit.ly/3CCmlfi">
+            <img
+              src={Avaliacao2}
+              alt="avaliação do perfil google meu negocio"
+              className="w-[650px]"
+            />
+          </a>
+          <a href="https://bit.ly/3CCmlfi">
+            <img
+              src={Avaliacao3}
+              alt="avaliação do perfil google meu negocio"
+              className="w-[650px]"
+            />
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
